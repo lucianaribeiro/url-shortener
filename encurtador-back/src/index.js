4ab3fc4cbc4ef674c76eb868056ae49825789f54
Luciana <albuquerqueluciana54@gmail.com>
2020-10-14 18:33:11 -0300

    Fix network error

--- encurtador-back/src/index.js
+++ encurtador-back/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://204.48.27.38:3001/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://204.48.27.38:3001');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Methods', ['PATCH', 'POST', 'GET', 'DELETE', 'PUT']);
   res.setHeader('Access-Control-Allow-Headers', ['Content-Type']);
